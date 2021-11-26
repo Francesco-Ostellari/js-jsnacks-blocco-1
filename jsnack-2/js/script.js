31c7328609f0edf48e4fb2312ed0121cca4ac39b
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-26 15:06:23 +0100

    add-ciclo-while

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,5 +1,7 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
+
+ciclo for
 const invitati = ['Giovanni', 'Luca', 'Leonardo', 'Martina', 'Emma', 'Veronica'];
 const nomeUtente = prompt ('Come ti chiami?');
 let find = false;
@@ -18,4 +20,23 @@ if (find) {
   console.log ('Non puoi partecipare alla festa');
 }
 
+// ciclo while
+const invitati = ['Giovanni', 'Luca', 'Leonardo', 'Martina', 'Emma', 'Veronica'];
+const nomeUtente = prompt('Come ti chiami?');
+let find = false;
+let index = 0;
 
+while (index < invitati.length) {
+  const element = invitati[index];
+  console.log(element, index);
+  index++;
+  if (element == nomeUtente) {
+    find = true;
+  }
+}
+
+if (find) {
+  console.log('Puoi partecipare alla festa');
+} else {
+  console.log('Non puoi partecipare alla festa');
+}
