2421e4bb173f6ea6ded3c2d589d6c4371b7aec9a
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-26 14:29:22 +0100

    add-ciclo-while

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,10 +1,24 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
 
+// // ciclo for
 let somma = 0;
 for (let index = 1; index <= 10; index++) {
 
   let numberUser = parseInt(prompt("inserisci 10 numeri, uno per volta"));
   console.log(numberUser);
   somma += numberUser;
+}
+console.log(somma);
+
+// ciclo while
+
+let somma = 0;
+let i = 1;
+
+while (i<=10) {
+  let numberUser = parseInt(prompt("inserisci 10 numeri, uno per volta"));
+  console.log(numberUser);
+  somma += numberUser;
+  i++;
 }
 console.log(somma);
