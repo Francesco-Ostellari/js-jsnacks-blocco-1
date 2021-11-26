4307ec140535fd31ac09c153d88b6a27f5ad19ec
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-26 15:02:38 +0100

    add-ciclo-for

---
+++ jsnack-2/js/script.js
@@ -0,0 +1,21 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const invitati = ['Giovanni', 'Luca', 'Leonardo', 'Martina', 'Emma', 'Veronica'];
+const nomeUtente = prompt ('Come ti chiami?');
+let find = false;
+
+for (let index = 0; index < invitati.length; index++) {
+  const element = invitati[index];
+  console.log(element, index);
+  if (element == nomeUtente) {
+    find = true;
+  }
+}
+
+if (find) {
+  console.log('Puoi partecipare alla festa');
+} else {
+  console.log ('Non puoi partecipare alla festa');
+}
+
+
